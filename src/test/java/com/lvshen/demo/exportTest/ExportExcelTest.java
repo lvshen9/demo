1cab8c6585d4937cac621efd32ae71b985151af7
lvshen <1587023453@qq.com>
2021-04-18 20:19:50 +0800

    EasyExcel 使用3

--- src/test/java/com/lvshen/demo/exportTest/ExportExcelTest.java
+++ src/test/java/com/lvshen/demo/exportTest/ExportExcelTest.java
@@ -1,6 +1,12 @@
 package com.lvshen.demo.exportTest;
 
 import cn.hutool.core.lang.Console;
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.enums.WriteDirectionEnum;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.fill.FillConfig;
+import com.alibaba.excel.write.metadata.fill.FillWrapper;
 import com.lvshen.demo.easyexcel.ExcelService;
 import com.lvshen.demo.easyexcel.TeacherInfoDto;
 import com.lvshen.demo.member.entity.Member;
@@ -13,7 +19,9 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Description:
@@ -26,7 +34,7 @@ import java.util.List;
 @Slf4j
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class Test {
+public class ExportExcelTest {
     @Autowired
     private MemberService memberService;
     @Autowired
@@ -52,5 +60,26 @@ public class Test {
         List<TeacherInfoDto> teacherInfoDtos = excelService.listInfo();
         excelService.exportInfo(fullName, sheetName, teacherInfoDtos);
         Console.log("导出结束");
+    }
+
+    @org.junit.Test
+    public void testFillExcel() {
+        List<TeacherInfoDto> teacherInfoDtos = excelService.listInfo();
+        String filePath = "D:/study/code/demo/src/test/java/com/lvshen/demo/exportTest/";
+        String templateFileName = filePath + "FillExcelTemplate.xlsx";
+
+        String fileName = filePath + "compositeFill" + System.currentTimeMillis() + ".xlsx";
+        ExcelWriter excelWriter = EasyExcel.write(fileName).withTemplate(templateFileName).build();
+        WriteSheet writeSheet = EasyExcel.writerSheet().build();
+        FillConfig fillConfig = FillConfig.builder().direction(WriteDirectionEnum.HORIZONTAL).build();
+        excelWriter.fill(new FillWrapper("data1", teacherInfoDtos), fillConfig, writeSheet);
+        excelWriter.fill(new FillWrapper("data2", teacherInfoDtos), writeSheet);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("date", "2021年4月18日19:34:28");
+        excelWriter.fill(map, writeSheet);
+
+        // 别忘记关闭流
+        excelWriter.finish();
     }
 }
