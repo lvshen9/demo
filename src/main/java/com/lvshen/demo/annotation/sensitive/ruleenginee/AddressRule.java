44ab96938a9455d383d0e72e681c730b3f89dce5
00759501 <1587023453@qq.com>
2021-04-30 17:04:59 +0800

    规则引擎2

--- src/main/java/com/lvshen/demo/annotation/sensitive/ruleenginee/AddressRule.java
+++ src/main/java/com/lvshen/demo/annotation/sensitive/ruleenginee/AddressRule.java
@@ -2,7 +2,10 @@ package com.lvshen.demo.annotation.sensitive.ruleenginee;
 
 import com.lvshen.demo.annotation.sensitive.SensitiveInfoUtils;
 import com.lvshen.demo.annotation.sensitive.SensitiveType;
+import org.jeasy.rules.annotation.Action;
+import org.jeasy.rules.annotation.Condition;
 import org.jeasy.rules.annotation.Fact;
+import org.jeasy.rules.annotation.Rule;
 import org.jeasy.rules.api.Facts;
 import org.springframework.stereotype.Component;
 
@@ -15,14 +18,17 @@ import org.springframework.stereotype.Component;
  * @since JDK 1.8
  */
 @Component
+@Rule(name = "AddressRule", description = "地址过滤规则")
 public class AddressRule implements SensitiveRule {
 
     @Override
+    @Condition
     public boolean isCurrentType(@Fact("ruleInfo") RuleEntity ruleInfo) {
         return SensitiveType.ADDRESS == ruleInfo.getType();
     }
 
     @Override
+    @Action
     public String maskingData(Facts facts) {
         RuleEntity ruleInfo = facts.get("ruleInfo");
         String infoStr = ruleInfo.getInfoStr();
